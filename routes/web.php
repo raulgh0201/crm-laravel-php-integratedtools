6c53f05352a04d4066c1f6e45249d5f4bf45ab4b
Raul <aplicacionwebm12@gmail.com>
2020-12-01 02:12:33 +0100

    v0.0

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.register');});
+    return view('auth.register');
+});
 Route::get('home', 'HomeController@home')->name('home');
 
 Auth::routes();
